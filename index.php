f32d8e727136f45c47d4ed35c226370b15469562
MrMangust <ninan2290@gmail.com>
2021-11-29 16:49:12 +0400

    ----

--- index.php
+++ index.php
@@ -3,13 +3,19 @@ require_once("index.html");
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST'){
 
-ECHO 'WELCOME';
-
-
-
 
 }
+/*$fname = trim($_SERVER['fname']);
+$lname = trim($_SERVER['lname']);
+$fname = trim($_SERVER['gender']);
+$fname = trim($_SERVER['email']);
+$fname = trim($_SERVER['password']);*/
 
-/*echo "<pre>";
+
+
+
+
+
+echo "<pre>";
 print_r($_REQUEST);
-echo "</pre>";*/
+echo "</pre>";

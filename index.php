03f47112307c49b0d881c3d227d99586aada37b3
MrMangust <ninan2290@gmail.com>
2021-11-29 09:26:30 +0400

    Готовая форма регистрации

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
 require_once("index.html");
-require_once("connect.php");
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+
+ECHO 'WELCOME';
+
+
+
+
+}
+
+/*echo "<pre>";
+print_r($_REQUEST);
+echo "</pre>";*/

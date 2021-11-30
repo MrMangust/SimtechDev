4cc12a5d583f80639dbd72bf0f61e83df95addcb
MrMangust <ninan2290@gmail.com>
2021-11-30 13:01:10 +0400

    For tests on other platform

--- index.php
+++ index.php
@@ -1,15 +1,25 @@
 <?php
 require_once("index.html");
+$host = 'localhost';
+$db_user = 'root';
+$db_password = 'root';
+$db_name = 'simtechdev';
+
+/*подключение к базе данных*/
+$mysqli = mysqli_connect($host, $db_user, $db_password,$db_name);
+
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+    $first_name = trim($_SERVER['first_name']);
+    $last_name = trim($_SERVER['last_name']);
+    $gender = trim($_SERVER['gender']);
+    $email = trim($_SERVER['email']);
+    $pass = password_hash(trim($_SERVER['password']), PASSWORD_BCRYPT);
 
+
+    $mysqli->query("INSERT INTO users (fname, lname, gender, email, password) VALUES ('$first_name','$last_name','$gender','$email','$pass')");
 
 }
-/*$fname = trim($_SERVER['fname']);
-$lname = trim($_SERVER['lname']);
-$fname = trim($_SERVER['gender']);
-$fname = trim($_SERVER['email']);
-$fname = trim($_SERVER['password']);*/
 
 
 

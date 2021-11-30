ab8cb294bdccb3beb950d27f538be1542c7041ed
MrMangust <ninan2290@gmail.com>
2021-11-30 16:13:33 +0400

    ---

--- index.php
+++ index.php
@@ -1,15 +1,18 @@
 <?php
 require_once("index.html");
-$host = 'localhost';
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+$servername= 'localhost';
 $db_user = 'root';
 $db_password = 'root';
 $db_name = 'simtechdev';
 
-/*подключение к базе данных*/
-$mysqli = mysqli_connect($host, $db_user, $db_password,$db_name);
+$mysqli = mysqli_connect($servername, $db_user, $db_password,$db_name);
+if (!$mysqli){
+    die("Connection failed: " . mysqli_connect_error());
+    }
 
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST'){
     $first_name = trim($_SERVER['first_name']);
     $last_name = trim($_SERVER['last_name']);
     $gender = trim($_SERVER['gender']);
@@ -17,15 +20,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST'){
     $pass = password_hash(trim($_SERVER['password']), PASSWORD_BCRYPT);
 
 
-    $mysqli->query("INSERT INTO users (fname, lname, gender, email, password) VALUES ('$first_name','$last_name','$gender','$email','$pass')");
+    $mysqli->query("INSERT INTO users (first_name, last_name, gender, email, password) VALUES ('$first_name','$last_name','$gender','$email','$pass')");
 
 }
-
-
-
-
-
-
-echo "<pre>";
-print_r($_REQUEST);
-echo "</pre>";

172ee374620a20507090fa6f877e20bbdba5f89b
MrMangust <ninan2290@gmail.com>
2021-12-05 19:27:00 +0400

    рабочая почта

--- feedback.php
+++ feedback.php
@@ -14,7 +14,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST'){
     $checkbox = trim($_REQUEST['checkbox']);
 
     $mysqli->query("INSERT INTO feedback (user_name, subject, email, phone, radio, textarea, asap, checkbox) VALUES ('$user_name', '$subject','$email', '$phone','$radio','$textarea','$asap','checkbox')");
-
+    mail("ninan2290@gmail.com",$subject, $textarea, );
     echo '<pre>';
     print_r($_POST);
     echo '</pre>';
